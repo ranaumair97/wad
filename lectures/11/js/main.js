8f5e546e78d003ae3f72345d4ac312b0c48f928e
Rana Umair <ranaumair9797@ucp.edu.pk>
2018-11-27 11:56:44 +0500

    add acticivity Solution

--- lectures/11/js/main.js
+++ lectures/11/js/main.js
@@ -185,3 +185,119 @@ HINT: use text-decoration:line-through; property of CSS
 
 */
 
+function getTodos() {
+    var todos = [];
+    var todos_str = localStorage.getItem('todo');
+    if(todos_str !== null)
+        todos = JSON.parse(todos_str);
+    return todos;
+}
+
+function add() {
+    var task = document.getElementById('task').value;
+    if(task.trim() == ''){
+        document.getElementById('message').style.display = 'block';
+        return false;
+    } else {
+        document.getElementById('message').style.display = 'none';
+    }
+    var todos = getTodos();
+    todos.push({task: task, isDone: false});
+    document.getElementById('task').value = '';
+    localStorage.setItem('todo',JSON.stringify(todos));
+    show();
+    return false;
+}
+
+function remove() {
+    var id = this.getAttribute('id');
+    var todos = getTodos();
+    todos.splice(id,1);
+    localStorage.setItem('todo',JSON.stringify(todos));
+    show();
+    return false;
+}
+
+function show() {
+    document.getElementById('todos').innerText = '';
+    var todos = getTodos();
+    var ul = document.createElement('ul');
+    ul.classList.add("list-group");
+    for(var i=0; i<todos.length; i++){
+        var li = document.createElement('li');
+        li.innerHTML  = '<li>' + todos[i].task + '</li>' +
+            '<button class="btn btn-danger" id="' + i + '">' +
+            '<i class="fa fa-trash-o"></i> ' +
+            '<span class="d-none d-sm-inline"> Delete </span> </button>' +
+			'<button onclick="change('+i+')" class="btn btn-info mr-2" id="' + i + '">' +
+            '<i class="fa fa-pencil"></i> ' +
+            '<span class="d-none d-sm-inline"> Mark As Done </span> </button>' +
+			'<button class="btn btn-success mr-2" id="' + i + '">' +
+            '<i class="fa fa-pencil"></i> ' +
+            '<span class="d-none d-sm-inline"> Edit </span> </button>' +
+			'<button onclick="edited('+i+')" class="btn btn-warning d-none mr-2" id="' + i + '">' +
+            '<i class="fa fa-pencil"></i> ' +
+            '<span class="d-none d-sm-inline"> Save </span> </button>' + 
+			'<input id="'+i+'" value="'+todos[i].task+'" class="form-control d-none">';
+			
+        li.classList.add("list-group-item");
+        if(todos[i].isDone)
+            li.classList.add("done");
+        ul.appendChild(li);
+    }
+    document.getElementById('todos').appendChild(ul);
+    var buttons = document.getElementsByClassName('btn-danger');
+    for(var i=0; i<buttons.length; i++){
+        buttons[i].addEventListener('click',remove);
+    }
+	 document.getElementById('todos').appendChild(ul);
+    var buttons = document.getElementsByClassName('btn-success');
+    for(var i=0; i<buttons.length; i++){
+        buttons[i].addEventListener('click',savechanges);
+    }
+	
+ 
+}
+
+function savechanges()
+{
+	var id = this.getAttribute('id');
+	var btn = document.querySelectorAll('button')[3].classList.add('d-none');
+	document.querySelectorAll('button')[4].classList.remove('d-none');
+	var input = document.querySelectorAll('input')[parseInt(id) + 1].classList.remove('d-none');
+	
+	
+}
+
+function edited(id) {
+	var todos = getTodos();
+	var input = document.querySelectorAll('input');
+	var elm = input[parseInt(id) + 1];
+	todos[id].task = elm.value;
+    localStorage.setItem('todo',JSON.stringify(todos));
+    show();
+    return false;	
+}
+
+function change(id) {
+	var todos = getTodos();
+    todos[id].isDone = true;
+    localStorage.setItem('todo',JSON.stringify(todos));
+    show();
+    return false;
+}
+
+function isDone(e) {
+    var todos = getTodos();
+    if(todos[e.target.id].isDone) {
+        e.target.classList.add('done');
+        todos[e.target.id].isDone = false;
+    }
+    else{
+        e.target.classList.remove('done');
+        todos[e.target.id].isDone = true;
+    }
+    localStorage.setItem('todo',JSON.stringify(todos));
+    show();
+}
+show();
